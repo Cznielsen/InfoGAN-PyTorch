abf0de75d4725739cf2890cd09d2f1ccc44be3f3
Christian Zhuang-Qing Nielsen <czqnielsen@live.dk>
2019-11-13 11:19:56 +0100

    made it run (does it work? who knows) on model similar to the one in paper. Still need to swap RELU and batchnorm

--- models/qd_model.py
+++ models/qd_model.py
@@ -12,11 +12,11 @@ class Generator(nn.Module):
 
         #self.tconv1 = nn.ConvTranspose2d(74, 1024, 1, 1, bias=False)
         self.fc1 = nn.Linear(74, 1024)
-        self.bn1 = nn.BatchNorm2d(1024)
+        self.bn1 = nn.BatchNorm1d(1024)
 
         #self.tconv2 = nn.ConvTranspose2d(1024, 128, 7, 1, bias=False)
         self.fc2 = nn.Linear(1024, 128*7*7)
-        self.bn2 = nn.BatchNorm2d(128*7*7)
+        self.bn2 = nn.BatchNorm2d(128)
 
         self.tconv1 = nn.ConvTranspose2d(128, 64, 4, 2, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(64)
@@ -24,9 +24,17 @@ class Generator(nn.Module):
         self.tconv2 = nn.ConvTranspose2d(64, 1, 4, 2, padding=1, bias=False)
 
     def forward(self, x):
+        x = x.view(-1, 74)
         x = F.relu(self.bn1(self.fc1(x)))
-        x = F.relu(self.bn2(self.fc2(x)))
-        x = F.relu(self.bn3(self.tconv1(x)))
+
+        x = self.fc2(x)
+        x = x.view(-1, 128, 7, 7) 
+        x = F.relu(self.bn2(x))
+        #x = F.relu(self.bn2(self.fc2))
+        x = self.tconv1(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        #x = F.relu(self.bn3(self.tconv1(x)))
 
         img = torch.tanh(self.tconv2(x))
 
@@ -43,11 +51,12 @@ class Discriminator(nn.Module):
 
         #self.conv3 = nn.Conv2d(128, 1024, 7, bias=False)
         self.fc1 = nn.Linear(128*7*7, 1024)
-        self.bn3 = nn.BatchNorm2d(1024)
+        self.bn3 = nn.BatchNorm1d(1024)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x), 0.1, inplace=True)
         x = F.leaky_relu(self.bn2(self.conv2(x)), 0.1, inplace=True)
+        x = x.view(-1, 128*7*7)
         x = F.leaky_relu(self.bn3(self.fc1(x)), 0.1, inplace=True)
 
         return x
@@ -55,13 +64,10 @@ class Discriminator(nn.Module):
 class DHead(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.conv = nn.Linear(1024, 1+2+10) #out_features = output_dimension + len(continous_latent_code) + len(discrete_latent_code) 
-                                            #Kan meget vel være at dette ikke er nødvendigt (og at out_features skal være = 1) fordi der bliver lavet noget lignende inde QHead
+        self.conv = nn.Linear(1024, 1)
 
     def forward(self, x):
         output = torch.sigmoid(self.conv(x))
-
         return output
 
 class QHead(nn.Module):
@@ -76,6 +82,7 @@ class QHead(nn.Module):
         self.conv_var = nn.Conv2d(128, 2, 1)
 
     def forward(self, x):
+        x = x.view(-1, 1024, 1, 1)
         x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
 
         disc_logits = self.conv_disc(x).squeeze()
